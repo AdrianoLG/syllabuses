0c64920ad2b7ed8331a7e8f3bec66867b862ee8c
AdrianoLG <adri@adri.info>
2016-08-07 15:42:12 +0200

    Added socketIO and styles

--- client/app/syllabus/syllabus.controller.ts
+++ client/app/syllabus/syllabus.controller.ts
@@ -2,12 +2,23 @@
 (function(){
 
     angular.module('04MeanApp')
-        .controller('SyllabusCtrl', function($scope, SyllabusService) {
+        .controller('SyllabusCtrl', function($scope, SyllabusService, socket) {
+
+            SyllabusService.query(function(syllabuses) {
+                $scope.syllabuses = syllabuses;
+                socket.syncUpdates('syllabus', $scope.syllabuses);
+            });
+
             $scope.addNewSyllabus = function() {
                 SyllabusService.save($scope.newSyllabus, function(syllabus) {
-                    console.log(syllabus);
+                    $scope.newSyllabus = {};
                 });
             };
+
+            $scope.$on('$destroy', function() {
+                socket.unsyncUpdates('syllabus');
+            });
+
         });
 
 })();
